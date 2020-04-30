0853dff37f5694c201e2ba0b09b0ae3483e9e3d1
dqhuy78 <dqhuy78@gmail.com>
2020-04-30 17:54:42 +0700

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
+import { GithubOutlined } from '@ant-design/icons';
 
 import 'assets/styles/tailwind.css';
 import 'assets/styles/app.css';
@@ -9,10 +10,20 @@ import DetailPage from 'pages/DetailPage';
 const App = () => {
     return (
         <div className="app w-100 h-screen overflow-hidden" style={{ backgroundColor: '#f7f8fe' }}>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/detail/:username" component={DetailPage} />
-            </Switch>
+            <div className="flex h-screen">
+                <div className="w-64 shadow-2xl flex flex-col justify-center items-center" style={{backgroundColor: '#24292e'}}>
+                    <span className="rounded-full inline-block" style={{backgroundColor: '#fff'}}>
+                        <GithubOutlined style={{fontSize: 90}} />
+                    </span>
+                    <p className="text-xl font-bold text-white mt-4">Github User Search</p>
+                </div>
+                <div className="flex flex-grow">
+                    <Switch>
+                        <Route exact path="/" component={HomePage} />
+                        <Route exact path="/detail/:username" component={DetailPage} />
+                    </Switch>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,84 +1,23 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import { Input, Avatar, Button, Spin, message } from 'antd';
+import { Input, Avatar, Button, Spin, Result } from 'antd';
 import { ClearOutlined } from '@ant-design/icons'
 
-import axios from 'utils/axios';
-
-const STATUS = {
-    LOADING: 'loading',
-    LOAD_SUCCESS: 'success',
-    LOAD_ERROR: 'error',
-}
-
-const TYPE = {
-    LOAD: 'LOAD',
-    LOAD_SUCCESS: 'LOAD_SUCCESS',
-    LOAD_ERROR: 'LOAD_ERROR',
-}
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case TYPE.LOAD:
-            return {
-                ...state,
-                status: STATUS.LOADING
-            };
-        case TYPE.LOAD_SUCCESS:
-            return {
-                ...state,
-                status: STATUS.LOAD_SUCCESS,
-                data: action.payload.data,
-                total: action.payload.total,
-            }
-        case TYPE.LOAD_ERROR:
-            return {
-                ...state,
-                status: STATUS.LOAD_ERROR,
-            }
-        default:
-            return state;
-    }
-}
+import useFetchDataHook from './useFetchDataHook'
 
 const HomePage = () => {
-    const [state, dispatch] = useReducer(reducer, {
-        status: STATUS.LOAD_SUCCESS,
-        data: [],
-        total: 0
-    });
+
     const [keyword, setKeyword] = useState('');
-    const [sourceToken, setSourceToken] = useState(null);
-
-
-    async function handleSearch(value) {
-        try {
-            if (value.trim() === '') {
-                return message.warning('Please enter some keyword to search');
-            }
-            if (sourceToken !== null) {
-                sourceToken.cancel();
-            }
-            dispatch({ type: TYPE.LOAD });
-            const CancelToken = axios.CancelToken;
-            console.log(CancelToken);
-            const source = CancelToken.source();
-            setSourceToken(source);
-            const { data } = await axios.get(`/search/users?q=${value}`, {
-                cancelToken: source.token
-            });
-            dispatch({
-                type: TYPE.LOAD_SUCCESS,
-                payload: {
-                    data: data.items,
-                    total: data.total_count
-                }
-            });
-        } catch (error) {
-            console.log(error);
-            dispatch({ type: TYPE.LOAD_ERROR });
-        }
-    }
+    const [
+        getData,
+        getTotal,
+        isLoading,
+        isLoadSuccess,
+        isLoadError,
+        clearSearch,
+        handleSearch,
+        handleLoadMore
+    ] = useFetchDataHook(keyword);
 
     function handleInputChange(e) {
         setKeyword(e.target.value)
@@ -86,13 +25,40 @@ const HomePage = () => {
 
     function handleClearSearch() {
         setKeyword('');
-        dispatch({
-            type: TYPE.LOAD_SUCCESS,
-            payload: {
-                data: [],
-                total: 0
+        clearSearch();
+    }
+
+    function render() {
+        if (isLoadError()) {
+            return <Result status="500" title="500" subTitle="Sorry, something went wrong." />
+        }
+        if (isLoadSuccess()) {
+            if (getTotal() === 0) {
+                return <Result title="No user found" />
+            } else {
+                return getData().map((user, index) => (
+                    <div key={user.id} className="flex items-center mb-6 pb-6 justify-between" style={{ borderBottom: '1px solid #ccc' }}>
+                        <div className="flex items-center">
+                            <p className="mr-8 mb-0">#{index + 1}</p>
+                            <Avatar src={user.avatar_url} size={48} />
+                            <div className="ml-4">
+                                <p className="font-bold text-base mb-0">
+                                    {user.login}
+                                </p>
+                                <a href={user.html_url} target="__blank" className="text-sm underline">
+                                    Github Link
+                                </a>
+                            </div>
+                        </div>
+                        <Button type="primary">
+                            <Link to={`/detail/${user.login}`}>
+                                Detail
+                            </Link>
+                        </Button>
+                    </div>
+                ));
             }
-        });
+        }
     }
 
     return (
@@ -112,40 +78,18 @@ const HomePage = () => {
                     Clear
                 </Button>
             </div>
-            <Spin size="large" tip="Loading" spinning={state.status === STATUS.LOADING} delay="200">
+            <Spin size="large" tip="Loading" spinning={isLoading()} delay="200">
                 <div
                     className="mt-4 px-4 py-8 shadow-lg rounded overflow-y-auto scroll"
                     style={{ backgroundColor: '#fff', width: 600, height: 'calc(100vh - 170px)' }}
                 >
-                    <p className="font-bold mb-6">Result found: {state.total}</p>
+                    <p className="font-bold mb-6">Result found: {getTotal()}</p>
+                    {render()}
                     {
-                        state.data.map((user, index) => (
-                            <div key={user.id} className="flex items-center mb-6 pb-6 justify-between" style={{ borderBottom: '1px solid #ccc' }}>
-                                <div className="flex items-center">
-                                    <p className="mr-8 mb-0">#{index + 1}</p>
-                                    <Avatar src={user.avatar_url} size={48} />
-                                    <div className="ml-4">
-                                        <p className="font-bold text-base mb-0">
-                                            {user.login}
-                                        </p>
-                                        <a href={user.html_url} target="__blank" className="text-sm underline">
-                                            Github Link
-                                        </a>
-                                    </div>
-                                </div>
-                                <Button type="primary">
-                                    <Link to={`/detail/${user.login}`}>
-                                        Detail
-                                    </Link>
-                                </Button>
-                            </div>
-                        ))
-                    }
-                    {
-                        state.data.length < state.total
+                        getData().length < getTotal()
                         && (
                             <div className="w-100 mt-4 flex justify-center">
-                                <Button type="primary">
+                                <Button type="primary" onClick={handleLoadMore}>
                                     Load More
                                 </Button>
                             </div>
